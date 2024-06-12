47bfafeedeadb49a090650ad855a89e0f56acd42
TimaxHack <47035777+TimaxHack@users.noreply.github.com>
2024-06-12 23:10:00 +0300

    хранение в дипе

--- src/api/chatgpt.js
+++ src/api/chatgpt.js
@@ -1,17 +1,8 @@
 
 import axios from 'axios';
-import path, { dirname } from 'path';
-import { saveTokensData, syncContextData, loadData, makeDeepClient, requestBody, deleteFirstMessage } from '../utils/dbManager.js';
-import { fileURLToPath } from 'url';
+import { syncContextData, updateTokensData, makeDeepClient, requestBody, deleteFirstMessage, selectTokensData } from '../utils/dbManager.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const tokensFilePath = path.join(__dirname, '..', 'db', 'tokens.json');
 const spaceIdArgument = process.env.SPACE_ID_ARGUMENT;
-const token = process.env.GQL_TOKEN;
-const GQL_URN = process.env.GQL_URN;
-const GQL_SSL = process.env.GQL_SSL === 'true';
 
 let role = "";
 
@@ -19,34 +10,27 @@ let role = "";
 const deploymentConfig = {
   'gpt-4o': {
       modelName: 'gpt-4o',
-      endpoint: 'https://deep-ai-west-us-3.openai.azure.com/',
+      endpoint: 'https://deep-ai-west-us-3.openai.azure.com',
       apiKey: process.env.AZURE_OPENAI_KEY,
-      apiVersion: '2024-05-13'
+      apiVersion: '2023-03-15-preview'
   },
   'gpt-35-turbo-16k': {
       modelName: 'gpt-35-turbo-16k',
-      endpoint: 'https://deep-ai.openai.azure.com/',
+      endpoint: 'https://deep-ai.openai.azure.com',
       apiKey: process.env.AZURE_OPENAI_KEY_TURBO,
       apiVersion: '2023-03-15-preview'
   }
 };
 
-async function queryChatGPT(userQuery, token, dialogName, model = 'gpt-4o', systemMessageContent = '', tokenLimit = Infinity, singleMessage = false) {
-  const deep = makeDeepClient(token);
+async function queryChatGPT(userQuery, token, dialogName, model = 'gpt-4o', systemMessageContent = '', tokenLimit = Infinity, singleMessage = true, userNameToken, token_GQL) {
+  const deep = makeDeepClient(token_GQL);
   const config = deploymentConfig[model] || deploymentConfig['gpt-4o']; // Если модель не найдена, используем gpt-4o
   if (!config) {
     console.error(`Deployment config for ${model} not found`);
     return;
   }
 
-  const validLimitToken = await loadData(tokensFilePath);
-  const tokenBounded = validLimitToken.tokens.find(t => t.token === token);
-  if (!tokenBounded || new Date(tokenBounded.expires) < new Date()) {
-    throw new Error('Токен не действителен или уже истек.');
-  }
-  if (tokenBounded.used.user > tokenBounded.limits.user || tokenBounded.used.chatGpt > tokenBounded.limits.chatGpt) {
-    throw new Error('Превышен лимит использования токенов.');
-  }
+  const validLimitToken = await selectTokensData(deep, userNameToken);
 
   if (!dialogName) {
     singleMessage = true;
@@ -55,43 +39,47 @@ async function queryChatGPT(userQuery, token, dialogName, model = 'gpt-4o', syst
   const systemMessage = { role: 'system', content: systemMessageContent || 'You are chatting with an AI assistant.' };
   role = "user";
   const userMessage = { role: 'user', content: userQuery };
-
   const messageAllContextUser = singleMessage ? [systemMessage, userMessage] : await syncContextData(dialogName, userMessage.content, role, systemMessage.content, spaceIdArgument, deep);
 
   try {
-    const endpointAll = `${config.endpoint}openai/deployments/${config.modelName}/chat/completions?api-version=${config.apiVersion}`;
-    const response = await axios.post(endpointAll, { messages: messageAllContextUser }, {
-      headers: { 'Content-Type': 'application/json', 'api-key': config.apiKey }
-    });
+    const endpointAll = `${config.endpoint}/openai/deployments/${config.modelName}/chat/completions?api-version=${config.apiVersion}`;
+  
+    console.log('Making request to endpoint:', endpointAll);  
 
+    const response = await axios.post(endpointAll, {
+      messages: messageAllContextUser,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'api-key': config.apiKey
+      }
+    });
     const gptReply = response.data.choices[0].message.content.trim();
     const requestTokensUsed = response.data.usage.prompt_tokens;
     const responseTokensUsed = response.data.usage.completion_tokens;
+    const token_user = validLimitToken[0] - requestTokensUsed;
+    const token_gpt = validLimitToken[1] - responseTokensUsed;
 
-    tokenBounded.used.user += requestTokensUsed;
-    tokenBounded.used.chatGpt += responseTokensUsed;
-    await saveTokensData(validLimitToken);
+    await updateTokensData(deep, userNameToken, token_user, token_gpt);
 
     if (!singleMessage) {
       // Логика обработки переполнения токенов
       const totalTokensUsed = requestTokensUsed + responseTokensUsed;
       if (totalTokensUsed > tokenLimit) {
-        await deleteFirstMessage(deep, dialogName, spaceIdArgument, /* нужное значение для удаления */);
+        await deleteFirstMessage(deep, dialogName, spaceIdArgument, 1);
       }
       role = "assistant";
       await syncContextData(dialogName, gptReply, role, systemMessage.content, spaceIdArgument, deep);
     }
 
-    const remainingUserTokens = tokenBounded.limits.user - tokenBounded.used.user;
-    const remainingChatGptTokens = tokenBounded.limits.chatGpt - tokenBounded.used.chatGpt;
 
     return {
       success: true,
       response: gptReply,
       requestTokensUsed,
       responseTokensUsed,
-      remainingUserTokens,
-      remainingChatGptTokens,
+      token_user,
+      token_gpt,
       history: await requestBody(deep, dialogName) // Получаем историю диалога
     };
   } catch (error) {
