761737695afbe379513d08b1a97c9bbbf7fda3e6
Grigoriy Gavrilov <lowww1337@mail.ru>
2024-09-24 16:34:37 +0300

    Добавит поддержку whisper

--- src/contollers/completionsController.js
+++ src/contollers/completionsController.js
@@ -11,44 +11,41 @@ const completionsController = express.Router();
 completionsController.post(
   "/v1/chat/completions",
   rest(async ({ req, res }) => {
-
-    console.log(`\n проверка токена`)
+    console.log(`\n проверка токена`);
     const tokenId = tokensService.getTokenFromAuthorization(req.headers.authorization);
     await tokensService.isAdminToken(tokenId);
     await tokensService.isHasBalanceToken(tokenId);
-    console.log(`\n токен проверен \n`)
-    console.log(tokenId)
+    console.log(`\n токен проверен \n`);
+    console.log(tokenId);
 
-    console.log(`\n задается тело запроса`)
+    console.log(`\n задается тело запроса`);
     const body = { ...req.body, stream_options: { include_usage: true } };
-
 
     const model = body.model;
     const stream = body.stream;
-    console.log(`\n модель: \n`)
-    console.log(model)
-    console.log(`\n параметр stream: \n`)
-    console.log(stream)
-
+    console.log(`\n модель: \n`);
+    console.log(model);
+    console.log(`\n параметр stream: \n`);
+    console.log(stream);
 
     if (!stream) {
       const completion = await completionsService.completions(body);
       const tokens = completion.usage.total_tokens;
       await completionsService.updateCompletionTokensByModel({ model, tokenId, tokens });
 
-      console.log(`\n без stream`)
-      console.log(`\n completion: \n`)
-      console.log(completion)
-      console.log(`\n tokens: \n`)
-      console.log(tokens)
+      console.log(`\n без stream`);
+      console.log(`\n completion: \n`);
+      console.log(completion);
+      console.log(`\n tokens: \n`);
+      console.log(tokens);
       return new HttpResponse(200, completion);
     }
 
     const completion = await completionsService.completions(body);
 
-    console.log(`\n использование stream`)
-    console.log(`\n completion: \n`)
-    console.log(completion)
+    console.log(`\n использование stream`);
+    console.log(`\n completion: \n`);
+    console.log(completion);
 
     return new SSEResponse(async () => {
       for await (const chunk of completion) {
@@ -57,8 +54,8 @@ completionsController.post(
         if (!chunk.usage) continue;
 
         const tokens = chunk.usage.total_tokens;
-        console.log(`\n tokens: \n`)
-        console.log(tokens)
+        console.log(`\n tokens: \n`);
+        console.log(tokens);
         await completionsService.updateCompletionTokensByModel({ model, tokenId, tokens });
       }
 
