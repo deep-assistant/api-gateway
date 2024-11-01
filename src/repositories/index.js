e326de1df1358c5b06b46c39d650522135791e5d
Grigoriy Gavrilov <lowww1337@mail.ru>
2024-11-01 19:15:37 +0300

    Рефакторинг

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -13,9 +13,9 @@ const __dirname = path.dirname(__filename);
 console.log(path.join(__dirname, '../db/tokens.json'))
 console.log(path.join(__dirname, '../db/referrals.json'))
 console.log(path.join(__dirname, '../db/dialogs.json'))
-const tokensDB = await JSONFilePreset(path.join(__dirname, '../db/tokens.json'), {tokens: []})
-const referralsDB = await JSONFilePreset(path.join(__dirname, '../db/referrals.json'), {tokens: []})
-const dialogsDB = await JSONFilePreset(path.join(__dirname, '../db/dialogs.json'), {tokens: []})
+const tokensDB = await JSONFilePreset("../db/tokens.json", {tokens: []})
+const referralsDB = await JSONFilePreset('../db/referrals.json', {tokens: []})
+const dialogsDB = await JSONFilePreset('../db/dialogs.json', {tokens: []})
 
 export const tokensRepository = new TokensRepository(tokensDB)
 export const referralRepository = new ReferralRepository(referralsDB)
