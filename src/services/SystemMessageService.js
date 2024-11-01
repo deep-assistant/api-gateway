7da58a3d6c6183c38c25a6330b3746d3fe31491a
Grigoriy Gavrilov <lowww1337@mail.ru>
2024-11-01 15:42:31 +0300

    Поправит ошибки запуска

--- src/services/SystemMessageService.js
+++ src/services/SystemMessageService.js
@@ -1,61 +1,54 @@
-import { fileURLToPath } from "url";
-import path from "path";
-import { loadData, saveData } from "../utils/dbManager.js";
-import { HttpException } from "../rest/HttpException.js";
+import {HttpException} from "../rest/HttpException.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const systemMessagesFilePath = path.join(__dirname, "../db", "systemMessages.json");
 
 export class SystemMessageService {
-  createSystemMessageEntity(userId, systemMessage) {
-    return {
-      userId,
-      systemMessage,
-    };
-  }
-
-  async getSystemMessage(id) {
-    const systemMessage = await this.getSystemMessageById(id);
-    if (!systemMessage) {
-      throw new HttpException(404, "Системное сообщение не найдено");
+    createSystemMessageEntity(userId, systemMessage) {
+        return {
+            userId,
+            systemMessage,
+        };
     }
 
-    console.log(systemMessage);
+    async getSystemMessage(id) {
+        const systemMessage = await this.getSystemMessageById(id);
+        if (!systemMessage) {
+            throw new HttpException(404, "Системное сообщение не найдено");
+        }
 
-    return systemMessage;
-  }
+        console.log(systemMessage);
 
-  async getSystemMessageById(userId) {
-    const { systemMessages } = await loadData(systemMessagesFilePath);
-    console.log(systemMessages);
-
-    return systemMessages.find((systemMessage) => systemMessage.userId === userId);
-  }
-
-  async createSystemMessage(userId, message) {
-    const { systemMessages } = await loadData(systemMessagesFilePath);
-
-    const foundSystemMessage = systemMessages.find((systemMessage) => systemMessage.userId === userId);
-    if (!foundSystemMessage) {
-      await saveData(systemMessagesFilePath, {
-        systemMessages: [...systemMessages, this.createSystemMessageEntity(userId, message)],
-      });
-      return await this.getSystemMessageById(userId);
+        return systemMessage;
     }
 
-    await saveData(systemMessagesFilePath, {
-      systemMessages: systemMessages.map((systemMessage) =>
-        systemMessage.userId === userId
-          ? {
-              ...systemMessage,
-              systemMessage: message,
-            }
-          : systemMessage,
-      ),
-    });
+    async getSystemMessageById(userId) {
+        const {systemMessages} = await loadData(systemMessagesFilePath);
+        console.log(systemMessages);
 
-    return await this.getSystemMessageById(userId);
-  }
+        return systemMessages.find((systemMessage) => systemMessage.userId === userId);
+    }
+
+    async createSystemMessage(userId, message) {
+        const {systemMessages} = await loadData(systemMessagesFilePath);
+
+        const foundSystemMessage = systemMessages.find((systemMessage) => systemMessage.userId === userId);
+        if (!foundSystemMessage) {
+            await saveData(systemMessagesFilePath, {
+                systemMessages: [...systemMessages, this.createSystemMessageEntity(userId, message)],
+            });
+            return await this.getSystemMessageById(userId);
+        }
+
+        await saveData(systemMessagesFilePath, {
+            systemMessages: systemMessages.map((systemMessage) =>
+                systemMessage.userId === userId
+                    ? {
+                        ...systemMessage,
+                        systemMessage: message,
+                    }
+                    : systemMessage,
+            ),
+        });
+
+        return await this.getSystemMessageById(userId);
+    }
 }
