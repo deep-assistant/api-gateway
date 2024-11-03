18ae467c34a113689347f9765855a956831c670c
Grigoriy Gavrilov <lowww1337@mail.ru>
2024-11-03 21:03:40 +0300

    Рефакторинг

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -10,6 +10,7 @@ import {DialogsRepository} from "./DialogsRepository.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+console.log(path.join(__dirname, '../db/tokens.json'))
 const tokensDB = await JSONFilePreset(path.join(__dirname, '../db/tokens.json'), {tokens: []})
 const referralsDB = await JSONFilePreset(path.join(__dirname, '../db/referrals.json'), {referrals: []})
 const dialogsDB = await JSONFilePreset(path.join(__dirname, '../db/dialogs.json'), {dialogs: []})
